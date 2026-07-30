f071dc1d41cfb9d9395b7903a1606f8d1a82cfee
marionette1994 <85476566+marionette1994@users.noreply.github.com>
2026-07-30 11:41:29 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+// TaskChute Mobile Service Worker
+const CACHE = 'taskchute-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS).catch(() => c.add('./index.html')))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// cache-first: オフラインでも起動、オンライン時は裏で更新
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+
+  e.respondWith(
+    caches.match(req, { ignoreSearch: true }).then((hit) => {
+      const fetching = fetch(req)
+        .then((res) => {
+          if (res && res.ok) {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+          }
+          return res;
+        })
+        .catch(() => hit);
+      return hit || fetching;
+    })
+  );
+});
